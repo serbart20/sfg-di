fa680c62a3eadfb27953dbf37b731035b56acd9d
serbart20 <serbart.dev@gmail.com>
2024-05-06 12:04:51 +0200

    moved Primary and Profile annotation in Java Configuration context

--- src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
+++ src/main/java/guru/springframework/sfgdi/config/GreetingServiceConfig.java
@@ -1,16 +1,39 @@
 package guru.springframework.sfgdi.config;
 
 import guru.springframework.sfgdi.services.ConstructorGreetingService;
+import guru.springframework.sfgdi.services.I18NSpanishService;
+import guru.springframework.sfgdi.services.I18nEnglishGreetingService;
+import guru.springframework.sfgdi.services.PrimaryGreetingService;
 import guru.springframework.sfgdi.services.PropertyInjectedGreetingService;
 import guru.springframework.sfgdi.services.SetterInjectedGreetingService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 
 /**
  * Created by jt on 2/20/21.
  */
 @Configuration
 public class GreetingServiceConfig {
+
+    @Profile({"ES", "default"})
+    @Bean("i18nService")
+    I18NSpanishService i18nSpanishService() {
+        return new I18NSpanishService();
+    }
+
+    @Profile("EN")
+    @Bean
+    I18nEnglishGreetingService i18nService() {
+        return new I18nEnglishGreetingService();
+    }
+
+    @Primary
+    @Bean
+    PrimaryGreetingService primaryGreetingService() {
+        return new PrimaryGreetingService();
+    }
 
     @Bean
     ConstructorGreetingService constructorGreetingService(){
--- src/main/java/guru/springframework/sfgdi/services/I18NSpanishService.java
+++ src/main/java/guru/springframework/sfgdi/services/I18NSpanishService.java
@@ -1,13 +1,13 @@
 package guru.springframework.sfgdi.services;
 
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Service;
+// import org.springframework.context.annotation.Profile;
+// import org.springframework.stereotype.Service;
 
 /**
  * Created by jt on 12/27/19.
  */
-@Profile({"ES", "default"})
-@Service("i18nService")
+// @Profile({"ES", "default"})
+// @Service("i18nService")
 public class I18NSpanishService implements GreetingService {
     @Override
     public String sayGreeting() {
