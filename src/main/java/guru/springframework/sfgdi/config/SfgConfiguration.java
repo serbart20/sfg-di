908b2b8c50991b43a0a94718d4923969c7432424
serbart20 <serbart.dev@gmail.com>
2024-05-07 15:03:35 +0200

    constructor properties binding

--- src/main/java/guru/springframework/sfgdi/config/SfgConfiguration.java
+++ src/main/java/guru/springframework/sfgdi/config/SfgConfiguration.java
@@ -9,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @ConfigurationProperties("guru")
 @Configuration
 public class SfgConfiguration {
+    
     private String username;
     private String password;
     private String jdbcurl;
